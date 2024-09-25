c5a322e375e8eac5f6350e32668c597b6f3d7e17
gilmar.penido <gilmar.penido@gmail.com>
2024-09-25 09:20:28 -0300

    parametros mgcloud

--- services/firebird.ts
+++ services/firebird.ts
@@ -2,7 +2,7 @@ var Firebird = require('node-firebird');
 
 var options = {
     host: '127.0.0.1',
-    database: 'C:\\firebird\\GERPROJ_SOLUTII.GDB',
+    database: 'E:\\GERPROJ\\GERPROJ_SOLUTII.GDB',
     user:  'SYSDBA',
     password: 'masterkey',
     pageSize: 4096,
